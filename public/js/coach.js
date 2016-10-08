e0bcd41c56f1214d71a830e5a52d1f787b56587b
marinaesca <marinaescalante@utexas.edu>
2016-10-08 14:34:35 -0500

    dummy read remove

--- public/js/coach.js
+++ public/js/coach.js
@@ -21,17 +21,3 @@ function readCoach(userId, callback)
     );
 }
 
-coachData1 = {"name": "Bob", "email": "bob@bob.com", "cellNumber": "1234567890", "schoolName": "FallField Elementary", "address": "1234 school lane"};
-
-coachData2 = {"name": "Cassie", "email": "Cassie@bob.com", "cellNumber": "1134567890", "schoolName": "WinterField Elementary", "address": "12344 school lane"};
-
-//writeCoach("12345", readCoach(coachData1));
-//writeCoach("23456", function(coachData2));
-writeCoach("12345", coachData1);
-readCoach("12345", function(data) {
-    console.log("coach info", data);
-});
-writeCoach("23456", coachData2);
-readCoach("23456", function(data) {
-    console.log("coach info", data);
-});
